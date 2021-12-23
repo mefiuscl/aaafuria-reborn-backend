503469edc9ea6e5afe4f43ef0e46204a7424c5b2
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-23 19:31:18 -0300

    admin

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,12 @@ import core.models as core_models
 
 @admin.register(core_models.Socio)
 class SocioAdmin(admin.ModelAdmin):
-    pass
+
+    @admin.action(description='Definir sócio como Staff')
+    def set_staff(self, request, queryset):
+        queryset.update(user__is_staff=True)
+
+    actions = [set_staff]
 
 
 @admin.register(core_models.Pagamento)
