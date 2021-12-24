9a15fe32e645c10c43ef595de1049624ad4ee24b
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-24 07:53:38 -0300

    user admin

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import UserAdmin
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm
 
 import core.models as core_models
 
@@ -15,11 +16,12 @@ class UserAdmin(admin.ModelAdmin):
         queryset.update(is_staff=True)
 
     actions = [set_staff]
+    fieldsets = UserAdmin.fieldsets
+    form = UserChangeForm
 
 
 @admin.register(core_models.Socio)
 class SocioAdmin(admin.ModelAdmin):
-
     pass
 
 
