65503f1f0505c084b093caab24faa827fb3a93c0
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-23 22:43:44 -0300

    accounts urls

--- reborn/urls.py
+++ reborn/urls.py
@@ -13,28 +13,9 @@ from bank.views import bank_webhook
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path("core_webhook/", csrf_exempt(core_webhook)),
     path("ecommerce_webhook/", csrf_exempt(ecommerce_webhook)),
     path("bank_webhook/", csrf_exempt(bank_webhook)),
-    path(
-        'admin/password_reset/',
-        auth_views.PasswordResetView.as_view(),
-        name='admin_password_reset'
-        ),
-    path(
-        'admin/password_reset/done/',
-        auth_views.PasswordResetDoneView.as_view(),
-        name='password_reset_done'
-        ),
-    path(
-        'reset/<uidb64>/<token>/',
-        auth_views.PasswordResetConfirmView.as_view(),
-        name='password_reset_confirm'
-        ),
-    path(
-        'reset/done/',
-        auth_views.PasswordResetCompleteView.as_view(),
-        name='password_reset_complete'
-        ),
 ]
