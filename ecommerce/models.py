ba578a8fc350dd254a0f91747acee9c46b0a68f2
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-27 22:24:32 -0300

    remove boleto payment

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -151,7 +151,7 @@ class Carrinho(models.Model):
                 } for produto in self.produtos.all()
             ],
             customer=self.user.socio.stripe_customer_id,
-            payment_method_types=['card', 'boleto'],
+            payment_method_types=['card'],
         )
 
         self.status = 'aguardando'
