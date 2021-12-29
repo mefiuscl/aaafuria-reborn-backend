14a497e202a41e227e97a1ac554a1652f8b52618
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-29 12:58:00 -0300

    file overwrite

--- reborn/storage_backends.py
+++ reborn/storage_backends.py
@@ -9,6 +9,7 @@ class StaticStorage(S3Boto3Storage):
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
+    file_overwrite = False
 
 
 class PrivateMediaStorage(S3Boto3Storage):
