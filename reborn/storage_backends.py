e1e7dd2f92afe06cb9bf899d620528616e016660
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-29 12:40:36 -0300

    file overwrite

--- reborn/storage_backends.py
+++ reborn/storage_backends.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
@@ -10,7 +9,6 @@ class StaticStorage(S3Boto3Storage):
 class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
-    file_overwrite = False
 
 
 class PrivateMediaStorage(S3Boto3Storage):
