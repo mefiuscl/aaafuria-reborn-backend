888e6335c9e1f62bd13ec1bdeac4d4f01d24e913
Leonardo Nunes <leonunesbs@gmail.com>
2021-12-29 18:59:23 -0300

    create conta signal

--- core/models.py
+++ core/models.py
@@ -135,6 +135,11 @@ class Socio(models.Model):
         if instance.avatar:
             instance.avatar.delete(save=False)
 
+    @receiver(models.signals.post_save, sender='core.Socio')
+    def create_socio_conta(sender, instance, created, **kwargs):
+        from bank.models import Conta
+        Conta.objects.get_or_create(socio=instance)
+
 
 class Pagamento(models.Model):
     socio = models.ForeignKey(
