47be472f25d2610ba0b9790f11e1e03543974dae
Leonardo Nunes <leonunesbs@gmail.com>
2022-02-16 15:17:21 -0300

    fix

--- core/models.py
+++ core/models.py
@@ -134,6 +134,11 @@ class Socio(models.Model):
 
         super().save(*args, **kwargs)
 
+        if self.stripe_customer_id and Socio.objects.filter(
+                stripe_customer_id=self.stripe_customer_id).count() > 1:
+            self.delete()
+            raise ValidationError('Já existe um usuário com esse email.')
+
     def delete(self, *args, **kwargs):
         super().delete(*args, **kwargs)
 
