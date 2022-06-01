4c03a974945a8822229a211ecce6463dd6829525
Leonardo Nunes <leonunesbs@gmail.com>
2022-06-01 13:09:32 -0300

    inline

--- bank/models.py
+++ bank/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 from graphql_relay import to_global_id
-from memberships.models import Membership
 
 API_KEY = settings.STRIPE_API_KEY
 
@@ -111,9 +110,7 @@ class Payment(models.Model):
         self.description = description
         self.status = 'PAGO'
 
-        membership = Membership.objects.filter(payment=self)
-        if membership.exists():
-            membership.first().refresh()
+        self.membership.refresh() if self.membership else None
 
         self.save()
 
