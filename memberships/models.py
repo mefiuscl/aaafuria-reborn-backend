eb2e3877cdbb158a0b229c5495f2ceb28373ba55
Leonardo Nunes <leonunesbs@gmail.com>
2022-06-01 22:02:43 -0300

    add date control fields

--- memberships/models.py
+++ memberships/models.py
@@ -27,6 +27,9 @@ class Membership(models.Model):
     current_end_date = models.DateField(blank=True, null=True)
     is_active = models.BooleanField(default=False)
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return f'{self.member} - {self.membership_plan}'
 
@@ -90,3 +93,6 @@ class MembershipPlan(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
