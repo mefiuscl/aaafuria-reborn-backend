36c3fe0f13ec93250a5ddd47273e0e39d87674b3
Leonardo Nunes <leonunesbs@gmail.com>
2022-06-07 12:10:40 -0300

    add is_active to membership_plan

--- memberships/models.py
+++ memberships/models.py
@@ -95,6 +95,7 @@ class MembershipPlan(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
